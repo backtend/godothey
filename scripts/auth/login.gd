6cc70cf1a76ec9b7f84302a016eb20f9ccac1473
backtend <backtend@gmail.com>
2026-07-22 02:53:52 +0800

    重构登录场景和脚本，优化节点路径；更新错误提示信息和按钮文本；新增游客登录功能

--- scripts/auth/login.gd
+++ scripts/auth/login.gd
@@ -1,32 +1,46 @@
 extends Control
 
-@onready var username_edit: LineEdit = $VBoxContainer/UsernameEdit
-@onready var password_edit: LineEdit = $VBoxContainer/PasswordEdit
-@onready var error_label: Label = $VBoxContainer/ErrorLabel
-@onready var login_button: Button = $VBoxContainer/LoginButton
+@onready var username_edit: LineEdit = $CenterContainer/PanelContainer/MarginContainer/VBoxContainer/UsernameEdit
+@onready var password_edit: LineEdit = $CenterContainer/PanelContainer/MarginContainer/VBoxContainer/PasswordEdit
+@onready var error_label: Label = $CenterContainer/PanelContainer/MarginContainer/VBoxContainer/ErrorLabel
+@onready var login_button: Button = $CenterContainer/PanelContainer/MarginContainer/VBoxContainer/LoginButton
+@onready var guest_button: Button = $CenterContainer/PanelContainer/MarginContainer/VBoxContainer/GuestButton
+
 
 func _ready() -> void:
     error_label.text = ""
+
     login_button.pressed.connect(_on_login_pressed)
+    guest_button.pressed.connect(_on_guest_pressed)
+
     Auth.login_success.connect(_on_login_success)
     Auth.login_failed.connect(_on_login_failed)
+
 
 func _on_login_pressed() -> void:
     var u := username_edit.text.strip_edges()
     var p := password_edit.text
-    print("正在登录")
+
     if u.is_empty() or p.is_empty():
-        error_label.text = "please input username and password"
-        #return
-        
+        error_label.text = "Please enter username and password."
+        return
+
     login_button.disabled = true
-    error_label.text = "登录中..."
+    error_label.text = "Signing in..."
+
     Auth.login(u, p)
 
+
+func _on_guest_pressed() -> void:
+    get_tree().change_scene_to_file("res://scenes/home/Home.tscn")
+
+
 func _on_login_success() -> void:
-    print("ready goto main sence...")
-    #get_tree().change_scene_to_file("res://scenes/main/Main.tscn")
-    get_tree().call_deferred("change_scene_to_file", "res://scenes/home/Home.tscn")
+    get_tree().call_deferred(
+        "change_scene_to_file",
+		"res://scenes/home/Home.tscn"
+    )
+
 
 func _on_login_failed(msg: String) -> void:
     login_button.disabled = false
