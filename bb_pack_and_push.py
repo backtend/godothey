7736fe461c297c544cf483242dcec0a0c95d22aa
backtend <backtend@gmail.com>
2026-07-24 04:13:07 +0800

    更新阿里云 OSS 配置，添加测试账号和密钥

--- bb_pack_and_push.py
+++ bb_pack_and_push.py
@@ -29,10 +29,10 @@ OUTPUT_DIR = PROJECT_ROOT / "docs/releases"
 PRIVATE_KEY_PEM = PROJECT_ROOT / "docs/signing/private_key.pem"
 
 # =========================================================
-# 阿里云 OSS 配置 
+# 阿里云 OSS 配置
 # =========================================================
-TESTAID = ""
-TESTAKY = ""
+TESTAID = "XXXXXXXXXXLTAI5t5bTvMz5HyN34px6G68"
+TESTAKY = "XXXXXXXXXXKIb6xcnxCbnhMCsYF3bLGOGvcn6Zi8"
 OSS_BUCKET = "yongit"
 OSS_ENDPOINT = "oss-cn-beijing.aliyuncs.com"
 OSS_BASE_URL = f"https://{OSS_BUCKET}.{OSS_ENDPOINT}"
