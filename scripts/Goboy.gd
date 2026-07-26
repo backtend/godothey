54237a4f2e77f575dcbec4ca78824237ee7043c8
backtend <backtend@gmail.com>
2026-07-26 19:20:34 +0800

    更改文件命名规范

--- scripts/Goboy.gd
+++ scripts/Goboy.gd
@@ -8,6 +8,12 @@ func _ready() -> void:
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
+    # 每秒钟打印一次delta时间
+    if delta > 999:
+        print("Delta time: ", delta)
+    # print("Delta time: ", delta)
+
+
     var moveY: float = Input.get_action_strength("down") - Input.get_action_strength("up")
     # print(    "Y轴的值是-value: " , moveY)
     position.y += moveY
